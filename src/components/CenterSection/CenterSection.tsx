67ed305b8c067f0e2ddc3980134fbf911ca2c53c
Manuel Reed <manny@pango.cc>
2026-04-16 12:05:17 -0400

    Added react-hook-form

--- src/components/CenterSection/CenterSection.tsx
+++ src/components/CenterSection/CenterSection.tsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import heroImg from "@assets/Points_PG_Dark.png";
 import "./styles.css";
 
+type FormData = {
+  salaryValue: number;
+  yearValue: number;
+};
+
 const CenterSection: React.FC = () => {
-  const [salaryValue, setSalaryValue] = useState<number>(0);
-  const [yearValue, setYearValue] = useState<number>(0);
   const yearOptions: number[] = [2019, 2020, 2021, 2022];
-
-  const handleSalaryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSalaryValue(Number(e.target.value));
-  };
-
-  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setYearValue(Number(e.target.value));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      salaryValue: Number("Enter Your Salary..."),
+      yearValue: yearOptions[0],
+    },
+    mode: "onChange",
+  });
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log("data: ");
+    console.log(data);
+    if (errors) {
+      console.log("error: ");
+      console.log(errors);
+    }
   };
 
   return (
@@ -22,15 +36,21 @@ const CenterSection: React.FC = () => {
       </div>
       <div>
         <h1>Tax Calculator</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
-            id="salary"
             type="number"
-            value={salaryValue}
-            onChange={handleSalaryChange}
+            step="0.01"
             placeholder="Enter Your Salary..."
+            {...register("salaryValue", {
+              required: true,
+              valueAsNumber: true,
+            })}
+            aria-invalid={errors.salaryValue ? "true" : "false"}
           />
-          <select name="team" value={yearValue} onChange={handleYearChange}>
+          <select
+            {...(register("yearValue"), { required: true })}
+            aria-invalid={errors.yearValue ? "true" : "false"}
+          >
             <option value="" disabled selected>
               Select Year
             </option>
@@ -38,7 +58,7 @@ const CenterSection: React.FC = () => {
               <option key={year}>{year}</option>
             ))}
           </select>
-          <button className="submit" onClick={() => {}}>
+          <button type="submit" className="submit">
             Submit
           </button>
         </form>
